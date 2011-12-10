4b0d01269d54c13f8d1cc554f4809698dc672fb2
r-isayev-parc <r.isayev@parcsis.org>
2011-12-10 22:53:29 +0300

    Collision add orient metod

--- collision/js/collision.js
+++ collision/js/collision.js
@@ -1,11 +1,29 @@
 var Collision = function() {
 	return {
+		orient: this.orient,
 		circle_intersect: this.circle_intersect 
 	};
 };
 	
 Collision.prototype = {
-
+	
+	// возвращает знак полуплоскости точки «c» относительно прямой (a1, a2).
+	// Если точка «c» лежит на прямой, то Orient() возвращает 0.
+	orient: function(a1, a2, c) {
+		var temp = (a1.x - c.x)*(a2.y - c.y) - (a1.y - c.y)*(a2.x - c.x);
+		
+		//$('#console').html(temp);
+		if (temp <= 0) {
+			if (temp = 0) {
+				return 0;
+			} else {
+				return -1;
+			}
+		} else {
+			return 1;
+		}	
+	},
+	
 	// возвращает true если окружности c центрами в с1 и с2 и
 	// радиусами r1 и r2 пересекаюстя, иначе - false			
 	circle_intersect: function(c1, r1, c2, r2) {
--- collision/js/func.js
+++ collision/js/func.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	var CANVAS_WIDTH = 640,
-		CANVAS_HEIGHT = 480,
+	var CANVAS_WIDTH = 1200,
+		CANVAS_HEIGHT = 900,
 		FPS = 30,
 		canvasElement = $('<canvas width="' + CANVAS_WIDTH + '" height="' + CANVAS_HEIGHT + '"></canvas>'),
 		canvas = canvasElement.get(0).getContext('2d'),
@@ -8,13 +8,15 @@ $(document).ready(function(){
 			x: 0,
 			y: 0
 		},
+		scene = ['default', 'orient', 'test'],
+		current_scene_index = 0,
 		collision = new Collision();
 		
 	canvasElement.appendTo('body');
 	
 	setInterval(function() {
 		update();
-		draw();
+		render();
 	}, 1000/FPS);
 	
 	// определяем координаты мыши на канве
@@ -29,55 +31,114 @@ $(document).ready(function(){
 	
 	var player = {
 		color: "#0066CC",
-		offset: {
+		position: {
 			x: 0,
 			y: 0	
 		},
+		offset: {
+			x: -30,
+			y: -30	
+		},
+		width: 60,
+		height: 60,
 		radius: 30,
 		draw: function() {
 			canvas.beginPath();
 			canvas.fillStyle = this.color;
-			canvas.arc(this.offset.x, this.offset.y, this.radius, 0, Math.PI*2, true);
+			switch (scene[current_scene_index]) {
+				case 'orient':
+					canvas.arc(this.position.x + this.offset.x, this.position.y + this.offset.y, 1, 0, Math.PI*2, true);
+					break
+				case 'test':
+					canvas.fillRect(this.position.x + this.offset.x, this.position.y + this.offset.y, this.width, this.height);
+					break
+				default:
+					canvas.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, true);
+			}	
+			canvas.stroke();
+			canvas.fill(); 
 			canvas.closePath();
-			canvas.fill(); 	
 		},
 		update: function() {
-			this.offset.x = mouse.x;
-			this.offset.y = mouse.y;
+			this.position.x = mouse.x;
+			this.position.y = mouse.y;
 		}
 	};
 	
 	var staticObject = {
 		color: "#A00",
-		offset: {
+		position: {
 			x: 300,
 			y: 200	
 		},
 		radius: 40,
-		draw: function() {
+		offset: {
+			x: -40,
+			y: -40	
+		},
+		width: 80,
+		height: 80,
+		draw: function() {			
 			canvas.beginPath();
 			canvas.fillStyle = this.color;
-			canvas.arc(this.offset.x, this.offset.y, this.radius, 0, Math.PI*2, true);
+			switch (scene[current_scene_index]) {
+				case 'orient':
+					canvas.lineWidth = 1;					
+					canvas.moveTo(this.position.x + this.offset.x, this.position.y + this.offset.y); // перемещаемся к координатам (x,y)
+					canvas.lineTo(this.position.x + this.offset.x + this.width, this.position.y + this.offset.y + this.height);				
+					break
+				case 'test':
+					canvas.fillRect(this.position.x + this.offset.x, this.position.y + this.offset.y, this.width, this.height);
+					break
+				default:
+					canvas.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, true);
+			}
+			canvas.stroke();
+			canvas.fill(); 
 			canvas.closePath();
-			canvas.fill(); 		
 		},
 		update: function() {
-			/*this.offset.x = mouse.x;
-			this.offset.y = mouse.y;*/
+			/*this.position.x = mouse.x;
+			this.position.y = mouse.y;*/
 		}
 	};
 	
 	var effect = {
-		color: "#FFFFCC",
+		color: ['PaleGreen', '#FFFFCC', 'lavender'],
+		current_color: 0,
 		display: false,
 		draw: function() {
 			if (this.display) {
-				canvas.fillStyle = this.color;
+				canvas.fillStyle = this.color[this.current_color];
 				canvas.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 			}
 		},
 		update: function() {
-			this.display = collision.circle_intersect(staticObject.offset, staticObject.radius, player.offset, player.radius);
+			switch (scene[current_scene_index]) {
+				case 'orient':
+					var a = {
+						x: staticObject.position.x + staticObject.offset.x,
+						y: staticObject.position.y + staticObject.offset.y
+					},
+					b = {
+						x: staticObject.position.x + staticObject.offset.x + staticObject.width,
+						y: staticObject.position.y + staticObject.offset.y	+ staticObject.height				
+					},
+					c = {
+						x: player.position.x + player.offset.x,
+						y: player.position.y + player.offset.y
+					},
+					orient = collision.orient(a, b, c);	
+					
+					this.current_color = orient < 0 ? 2 : orient;				
+					this.display = true;					
+					break
+				case 'test':
+					//
+					break
+				default:
+					this.display = collision.circle_intersect(staticObject.position, staticObject.radius, player.position, player.radius);
+			}
 		}
 	};
 	
@@ -89,11 +150,26 @@ $(document).ready(function(){
 		player.update();
 	}
 	
-	function draw() {
+	function render() {
 		canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		effect.draw();
 		staticObject.draw();
 		player.draw();
+		
+		// выводим название сцены
+		canvas.fillStyle = '#000';
+		canvas.font = '12px courier-new';
+		canvas.textBaseline = 'top';
+		canvas.fillText (scene[current_scene_index], 5, 5);
 	}
+	
+	// смена сцен по клику
+	$('canvas:first').click(function() {
+		if (current_scene_index < scene.length - 1) {
+			current_scene_index++
+		} else {
+			current_scene_index = 0;
+		}
+	});
 	
 });
